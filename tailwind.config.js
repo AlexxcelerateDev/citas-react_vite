a1d290bc9413f6cda30365cd24ee827e5cd21c16
Tu Nombre <tu_email@example.com>
2023-12-08 23:07:01 -0600

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["index.html","./src/**/*.jsx"], //** afecta a todos los archivos, *filtra
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
