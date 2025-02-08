1b7d3695df7664638206aebe706cc34a35f67507
AlexxcelerateDev <126349548+AlexxcelerateDev@users.noreply.github.com>
2025-02-07 22:07:36 -0600

    Update ListadoPacientes.jsx

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -27,7 +27,7 @@ function ListadoPacientes({ pacientes, setPaciente, eliminarPaciente }) {
             <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
             <p className="text-xl mt-5 mb-10 text-center" >
               Comienza agregando pacientes y
-              {' '}
+              {'a '}
               <span className="text-indigo-600 font-bold">aparecerán en este lugar</span>
             </p>
           </>
